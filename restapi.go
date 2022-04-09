26470c57e3103b40d0f2d828551be75fada58776
MistahJ <26472282+mistahj67@users.noreply.github.com>
2022-04-09 13:20:36 -0700

    discord api call metric

--- restapi.go
+++ restapi.go
@@ -15,6 +15,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/warmind-io/warmind/util"
 	"image"
 	_ "image/jpeg" // For JPEG decoding
 	_ "image/png"  // For PNG decoding
@@ -103,6 +104,9 @@ func (s *Session) request(method, urlStr, contentType string, b []byte, bucketID
 	if bucketID == "" {
 		bucketID = strings.SplitN(urlStr, "?", 2)[0]
 	}
+
+	util.IncrMetric("dbot.discord.apicall", []string{"source:dgo"}, 1)
+
 	return s.RequestWithLockedBucket(method, urlStr, contentType, b, s.Ratelimiter.LockBucket(bucketID), sequence, reason)
 }
 
